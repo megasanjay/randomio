453ae0f3c6889f63c17ab199c6446831b6e76f6e
Sanjay Soundarajan <sanjay.16@live.com>
2023-06-08 18:20:13 -0700

    cI: revert

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,8 @@
-import fastify from "fastify";
-import router from "./router";
-
-const server = fastify({
-  // Logger only for production
-  logger: !!(process.env.NODE_ENV !== "development"),
-});
-
-// Middleware: Router
-server.register(router);
+import app from "./app";
 
 const FASTIFY_PORT = Number(process.env.FASTIFY_PORT) || 3000;
 
-server.listen({ port: FASTIFY_PORT });
+app.listen({ port: FASTIFY_PORT });
 
 console.log(`🚀  Fastify server running on port ${FASTIFY_PORT}`);
 console.log(`Route index: /`);
