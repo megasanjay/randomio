fc155d8e9c3e307959e566299c89c08a330bf509
Sanjay Soundarajan <sanjay.16@live.com>
2023-06-08 19:55:14 -0700

    ci: update ci script

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import app from "./app";
 
 const FASTIFY_PORT = Number(process.env.FASTIFY_PORT) || 3000;
+const FASTIFY_HOST = process.env.FASTIFY_HOST || "0.0.0.0";
 
-app.listen({ port: FASTIFY_PORT });
+app.listen({ port: FASTIFY_PORT, host: FASTIFY_HOST });
 
 console.log(`🚀  Fastify server running on port ${FASTIFY_PORT}`);
 console.log(`Route index: /`);
